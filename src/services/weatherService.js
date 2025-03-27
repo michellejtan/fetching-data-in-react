50e284365c241b88b7bcaa9d0778ba2308e72100
michellejtan <michelle0223@gmail.com>
2025-03-27 13:34:40 -0700

    wip: creates weather service module

---
+++ src/services/weatherService.js
@@ -0,0 +1,18 @@
+const API_KEY = '';
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+      const queryString = `&q=${city}`;
+      const res = await fetch(BASE_URL + queryString);
+      const data = await res.json();
+      console.log('Data:', data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// show('New York');
+
+export { show }; // "named" export syntax
