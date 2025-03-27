ce3cd84393dde4437e63636efbaf1f3a56a7ab0a
michellejtan <michelle0223@gmail.com>
2025-03-27 13:40:38 -0700

    wip: imports and utilizes service function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import * as weatherService from './services/weatherService';
+import './App.css'
+
 const App = () => {
+  const fetchData = async () => {
+    const data = await weatherService.show('New York');
+    console.log('Data:', data);
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
 }
 
